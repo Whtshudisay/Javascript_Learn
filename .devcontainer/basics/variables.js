36f3459dbd89d872ccc78600df4132773441fe9f
Yash Avjekar <148756556+Whtshudisay@users.noreply.github.com>
2025-01-16 16:45:19 +0000

    container config done

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 1456383
+let accountEmail = "yashavjekar@gmail.com"
+var accountPassword = "yash2455"
+acountCity = "Bengaluru"
+
+
+console.log(accountEmail);
+console.table([accountEmail , accountId ,accountPassword, acountCity])
