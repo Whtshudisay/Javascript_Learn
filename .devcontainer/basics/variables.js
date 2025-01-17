1f6c49913c1470384a9f2e68ce150e285bd8899f
Whtshudisay <148756556+Whtshudisay@users.noreply.github.com>
2025-01-17 14:47:11 +0530

    container config done

--- .devcontainer/basics/variables.js
+++ .devcontainer/basics/variables.js
@@ -2,7 +2,11 @@ const accountId = 1456383
 let accountEmail = "yashavjekar@gmail.com"
 var accountPassword = "yash2455"
 acountCity = "Bengaluru"
-
+let accountState;
 
 console.log(accountEmail);
-console.table([accountEmail , accountId ,accountPassword, acountCity])
+console.table([accountEmail, accountState, accountId, accountPassword, acountCity])
+console.log(accountState);
+
+    
+//  prefer not use var keyword, use let instead. var is a global variable and let is a local variable.
