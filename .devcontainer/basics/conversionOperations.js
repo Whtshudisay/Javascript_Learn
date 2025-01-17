5b3a89273f6cbb8ea4097d6b56d929a82855e0ea
Whtshudisay <148756556+Whtshudisay@users.noreply.github.com>
2025-01-17 20:14:24 +0530

    Conversion Operations

---
+++ .devcontainer/basics/conversionOperations.js
@@ -0,0 +1,31 @@
+console.log("yml");
+
+let score = "4cwvcy";
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+let valueInString = String(score);
+console.log(valueInString);
+console.log(typeof valueInString);
+
+let valueInBoolean = Boolean(score);
+console.log(valueInBoolean);
+console.log(typeof valueInBoolean);
+
+// "33" => 33
+// 33 => "33"
+// "33" => true
+// 0 => false
+// 1 => true
+// "" => false
+// " " => true
+// "0" => true
+// "false" => true
+// "true" => true
+// "undefined" => true
+// "null" => true
+// "NaN" => true
+// "Infinity" => true 
