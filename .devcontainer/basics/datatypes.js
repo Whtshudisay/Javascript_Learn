8b3d0e02e2ccc99942c5c6c1e9176e7f9411e80c
Whtshudisay <148756556+Whtshudisay@users.noreply.github.com>
2025-01-17 15:57:36 +0530

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,16 @@
+console.log("yml");
+
+// alert("Hello, World!"); we are using alert to display the message in the browser but we are using Node.js
+
+console.log(3 + 9);
+
+console.log(typeof null); // null is an object
+console.log(typeof undefined); // undefined is undefined
+
+// bigInt
+// String
+// boolean
+// null => standalone data type
+// undefined
+// symbol => unique identifier
+// objects 
