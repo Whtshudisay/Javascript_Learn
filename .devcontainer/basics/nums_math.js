4f62795df5aea68fd03ff355a9faecaa5f0bfc4e
Whtshudisay <148756556+Whtshudisay@users.noreply.github.com>
2025-01-23 17:35:51 +0000

    Numbers and Maths

--- .devcontainer/basics/nums_math.js
+++ .devcontainer/basics/nums_math.js
@@ -1,9 +1,32 @@
 const score = 100;
 const bonus = new Number(20);
+const otherNumber = 23.58578528
 
 // console.log(score + bonus); 
-console.log(score);
-console.log(bonus);
+// console.log(score);
+// console.log(bonus);
 
-console.log(score.toFixed(2));
-console.log(bonus.toString().length);
+// console.log(score.toFixed(2));
+// console.log(bonus.toString().length);
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000003972937;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math.round(523851.1963418));
+// console.log(Math.random());
+// console.log(Math.ceil(36.234));
+// console.log(Math.floor(34.986));
+// console.log(Math.sqrt(144));
+// console.log(Math.min(328765, 83275, 2375, 3176., 61823));
+// console.log(Math.max(756, 3, 6, 357, 43, 6, 6,, 4, 63, 4, 87, 56));
+
+// console.log(Math.random());
+console.log(Math.floor(Math.random() * 10 ) +1 ) // non-zero
+
+
+const max = 100;
+const min = 1;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
