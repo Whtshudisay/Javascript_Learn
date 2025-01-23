ef5fcd185a3cb7884433c999d2fc91e6fd355aab
Whtshudisay <148756556+Whtshudisay@users.noreply.github.com>
2025-01-23 17:11:16 +0000

    container config done

--- .devcontainer/basics/nums&math.js
+++
@@ -1,6 +0,0 @@
-const score = 100;
-const bonus = new Number(20);
-
-console.log(score + bonus); 
-console.log(score);
-console.log(bonus);
--- .devcontainer/basics/nums_math.js
+++ .devcontainer/basics/nums_math.js
@@ -4,3 +4,6 @@ const bonus = new Number(20);
 // console.log(score + bonus); 
 console.log(score);
 console.log(bonus);
+
+console.log(score.toFixed(2));
+console.log(bonus.toString().length);
