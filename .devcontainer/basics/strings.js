dc905e8f7191ec085876fa79c6bfb17b4f017eb2
Whtshudisay <148756556+Whtshudisay@users.noreply.github.com>
2025-01-23 12:20:43 +0000

    Strings

---
+++ .devcontainer/basics/strings.js
@@ -0,0 +1,10 @@
+let name = 'YaSh';
+let age = 23;
+let isOnline = true;
+
+let str1 = `My name is {name}, my age is {age}, and I am {isOnline ? 'online' : 'offline'}`;
+console.log(str1);
+
+let str = new String('yash');// use it on web pages
+console.log(str[2]);
+
