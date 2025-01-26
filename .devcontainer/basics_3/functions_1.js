182ac0ca9d8663201c9d54de467a3d6e3046493e
Whtshudisay <148756556+Whtshudisay@users.noreply.github.com>
2025-01-26 10:11:26 +0000

    Functions

---
+++ .devcontainer/basics_3/functions_1.js
@@ -0,0 +1,24 @@
+function sayMyName() {
+    console.log('SHREE RAM');
+}
+
+function saymyName(name) {
+    console.log(name);
+}
+
+function returnString(name) {
+    return name;
+}
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+function arrFunction(arr) {
+    console.log(arr[1]);
+    console.log(`the length of arr is ${arr.length} and its second element is ${arr[1]}`);
+}
+
+let arr = [12, 54, 5, 67, 47, 468, 11];
+arrFunction(arr);
+
