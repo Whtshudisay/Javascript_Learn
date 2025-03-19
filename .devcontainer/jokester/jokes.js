6cb8c5e1e92c1f62404d2f5b158766f2c793173f
Whtshudisay <148756556+Whtshudisay@users.noreply.github.com>
2025-03-19 09:26:32 +0000

    jokes

---
+++ .devcontainer/jokester/jokes.js
@@ -0,0 +1,9 @@
+const jokes = require('give-me-a-joke');
+
+jokes.getRandomDadJoke(function (joke) {
+    console.log(joke);
+});
+
+jokes.getRandomCNJoke(function (joke) {
+    console.log(joke);
+});
